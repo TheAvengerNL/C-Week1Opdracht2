b0f643aaab0722d5a71cff093f20b5d6601a1113
TheAvengerNL <35960845+TheAvengerNL@users.noreply.github.com>
2018-11-18 14:50:18 +0100

    Question klasse is af voor het afhandelen van normale open vragen.

--- Quiz/Quiz/Program.cs
+++ Quiz/Quiz/Program.cs
@@ -2,11 +2,35 @@
 
 namespace Quiz
 {
-    class Program
+    class Quiz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Question open = new Question {Text = "Who was the inventor of Java?", Answer = "James Gosling"};
+            open.Display();
+            Console.Write("response: "); String response = Console.ReadLine();
+            Console.WriteLine(open.CheckAnswer(response)); Console.ReadLine();
         }
+    }
+
+    class Question
+    {
+        public String Text { get; set; }
+        public String Answer { get; set; }
+
+        public Boolean CheckAnswer(String response)
+        {
+            return response.ToLower().Equals(Answer.ToLower());
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(Text);
+        }
+    }
+
+    class MultipleChoice : Question
+    {
+
     }
 }
